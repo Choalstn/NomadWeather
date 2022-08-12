ea4c4f5d198cfa88b421ed250036c151e118f7c1
Choalstn <choalstn@gmail.com>
2022-08-12 19:03:33 +0900

    Location

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import reactDom from 'react-dom';
-import React from "react"; 
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
 import {View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
 
 const {width : SCREEN_WIDTH} = Dimensions.get('window');
 
 export default function App() {
+  const [city, setCity] = useState("Loading...")
+  const [ok, setOk] = useState(true);
+  const getWeather = async() => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if(!granted) {
+      setOk(false);
+    }
+
+    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy:5 });
+    const location = await  Location.reverseGeocodeAsync({latitude, longitude }, {useGoogleMaps:false});
+    setCity(location[0].street);
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, [])
+
   return (
     <View style={style.container}>
       <View style={style.city}>
-        <Text style={style.cityName}>Seoul</Text>
+        <Text style={style.cityName}>{city}</Text>
       </View>
 
       <ScrollView 
@@ -57,7 +76,7 @@ const style = StyleSheet.create({
    cityName : {
     marginTop: 20,
     fontSize: 58,
-    fontWeight: 'bold',
+    fontWeight: '900',
    },
 
    weather : {
@@ -70,7 +89,8 @@ const style = StyleSheet.create({
 
    temp : {
     marginTop: 50,
-    fontSize: 170,
+    fontSize: 150,
+    fontWeight: '500'
    },
 
    desc : {
